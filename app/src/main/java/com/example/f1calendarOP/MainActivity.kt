5d8241c9ad907b90166fbd8e8fabbe094ec66683
Olegs Pliska <olegpliska1@gmail.com>
2022-04-14 18:59:25 +0300

    Creating fragments

--- app/src/main/java/com/example/f1calendarOP/MainActivity.kt
+++ app/src/main/java/com/example/f1calendarOP/MainActivity.kt
@@ -1,109 +1,21 @@
 package com.example.f1calendarOP
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity(), RacesAdapter.MyOnClickListener{
-    private val raceList = ArrayList<RaceF1>()
-    private val racesAdapter: RacesAdapter by lazy { RacesAdapter(this) }
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         title = "F1 Calendar"
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
-        val layoutManager = LinearLayoutManager(applicationContext)
-
-        recyclerView.layoutManager = layoutManager
-        recyclerView.itemAnimator = DefaultItemAnimator()
-        recyclerView.adapter = racesAdapter
-        prepareRaceData()
-    }
-    //F1 2022 Calendar Info
-    private fun prepareRaceData() {
-        var race = RaceF1("18-20 MAR", "Bahrain Grand Prix", R.drawable.flag_bahrain,
-        "15:00", "18:00", "15:00", "18:00","18:00")
-        raceList.add(race)
-        race = RaceF1("25-27 MAR", "Jeddah Grand Prix", R.drawable.flag_saudi,
-        "17:00", "20:00", "17:00", "20:00", "20:00")
-        raceList.add(race)
-        race = RaceF1("08-10 APR", "Melbourne Grand Prix", R.drawable.flag_australia,
-        "06:00","09:00","06:00","09:00","08:00")
-        raceList.add(race)
-        race = RaceF1("22-24 APR", "Imola Grand Prix", R.drawable.flag_italy,
-        "14:30", "18:00","13:30","17:30","16:00")
-        raceList.add(race)
-        race = RaceF1("06-08 MAY", "Miami Grand Prix", R.drawable.flag_us,
-        "21:30","00:30","20:00","23:00","22:30")
-        raceList.add(race)
-        race = RaceF1("20-22 MAY", "Catalunya Grand Prix", R.drawable.flag_spain,
-        "15:00","18:00","14:00","17:00","16:00")
-        raceList.add(race)
-        race = RaceF1("27-29 MAY", "Monaco Grand Prix", R.drawable.flag_monaco,
-        "15:00","18:00","14:00","17:00","16:00")
-        raceList.add(race)
-        race = RaceF1("10-12 JUN", "Azerbaijan Grand Prix", R.drawable.flag_azerbaijan,
-        "14:00","17:00","14:00","17:00","14:00")
-        raceList.add(race)
-        race = RaceF1("17-19 JUN", "Canadian Grand Prix", R.drawable.flag_canada,
-        "21:00", "00:00","20:00","23:00", "21:00")
-        raceList.add(race)
-        race = RaceF1("01-03 JUL", "Silverstone Grand Prix", R.drawable.flag_uk,
-        "15:00","18:00", "14:00", "17:00", "17:00")
-        raceList.add(race)
-        race = RaceF1("08-10 JUL", "Red Bull Ring Grand Prix", R.drawable.flag_austria,
-        "14:30","18:00","13:30", "17:30","16:00")
-        raceList.add(race)
-        race = RaceF1("22-24 JUL", "French Grand Prix", R.drawable.flag_france,
-        "15:00", "18:00","14:00", "17:00", "16:00")
-        raceList.add(race)
-        race = RaceF1("29-31 JUL", "Hungarian Grand Prix", R.drawable.flag_hungary,
-        "15:00", "18:00", "14:00", "17:00", "16:00")
-        raceList.add(race)
-        race = RaceF1("26-28 AUG", "Spa-francorchamps Grand Prix", R.drawable.flag_belgium,
-        "15:00", "18:00","14:00", "17:00", "16:00")
-        raceList.add(race)
-        race = RaceF1("02-04 SEP", "Zandvoort Grand Prix", R.drawable.flag_netherlands,
-        "15:00", "18:00", "14:00", "17:00", "16:00")
-        raceList.add(race)
-        race = RaceF1("09-11 SEP", "Monza Grand Prix", R.drawable.flag_italy,
-        "15:00", "18:00","14:00", "17:00", "16:00")
-        raceList.add(race)
-        race = RaceF1("30-02 SEP-OCT", "Singapore Grand Prix", R.drawable.flag_singapore,
-        "13:00", "16:00", "13:00", "16:00", "15:00")
-        raceList.add(race)
-        race = RaceF1("07-09 OCT", "Suzuka Circuit Grand Prix", R.drawable.flag_japan,
-        "07:00", "10:00", "07:00", "10:00", "08:00")
-        raceList.add(race)
-        race = RaceF1("21-23 OCT", "United States Grand Prix", R.drawable.flag_us,
-        "22:00", "01:00", "22:00", "01:00", "22:00")
-        raceList.add(race)
-        race = RaceF1("28-30 OCT", "Mexico Grand Prix", R.drawable.flag_mexico,
-        "21:00", "00:00", "20:00", "23:00", "22:00")
-        raceList.add(race)
-        race = RaceF1("11-13 NOV", "Brazil Grand Prix",  R.drawable.flag_brazil,
-        "17:30", "21:00", "17:30", "21:30", "20:00")
-        raceList.add(race)
-        race = RaceF1("18-20 NOV", "Abu Dhabi Grand Prix", R.drawable.flag_uae,
-        "12:00", "15:00", "13:00", "16:00", "15:00")
-        raceList.add(race)
-        racesAdapter.updateList(raceList)
-    }
-    //Onn click function transferring to new activity with sent data
-    override fun OnClick(race: RaceF1, position: Int) {
-        val intent = Intent(this, RaceDetailActivity::class.java)
-        intent.putExtra(RACE_DATE_KEY, race)
-        startActivity(intent)
-    }
-
-    companion object{
-        const val RACE_DATE_KEY = "RACETRACK"
+        setContentView(R.layout.activity_main)
+        val raceListFragment = RaceListFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, raceListFragment)
+            commit()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -118,5 +30,4 @@ class MainActivity : AppCompatActivity(), RacesAdapter.MyOnClickListener{
         }
         return true
     }
-
 }
--- app/src/main/java/com/example/f1calendarOP/RaceDetailFragment.kt
+++ app/src/main/java/com/example/f1calendarOP/RaceDetailFragment.kt
@@ -1,42 +1,41 @@
 package com.example.f1calendarOP
 
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import androidx.fragment.app.Fragment
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
-import androidx.annotation.RequiresApi
+import com.example.f1calendarOP.RaceListFragment.Companion.RACE_DATE_KEY
 import java.text.SimpleDateFormat
 import java.util.*
 
-class RaceDetailActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_race_detail)
+class RaceDetailFragment : Fragment(R.layout.fragment_race_detail) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_race_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val race = arguments?.getSerializable(RACE_DATE_KEY) as RaceF1
         var testString : String?
-        val actionbar = supportActionBar
-        val race = intent.getSerializableExtra(MainActivity.RACE_DATE_KEY) as RaceF1
-        actionbar!!.title = "Race week"
         //binding variables to textviews
-        val trackDetailF1: TextView = findViewById(R.id.trackDetailF1) // Track name
-        val trackDateF1: TextView = findViewById(R.id.dateF1_2) // Date of GP weekend
-        val flagDetailF1: ImageView = findViewById(R.id.flagDetailF1) //Flag resource
-        val session1Date : TextView = findViewById(R.id.session1Date)
-        val session2Date : TextView = findViewById(R.id.session2Date)
-        val session3Date : TextView = findViewById(R.id.session3Date)
-        val session4Date : TextView = findViewById(R.id.session4Date)
-        val session5Date : TextView = findViewById(R.id.session5Date)
+        val trackDetailF1: TextView = view.findViewById(R.id.trackDetailF1) // Track name
+        val trackDateF1: TextView = view.findViewById(R.id.dateF1_2) // Date of GP weekend
+        val flagDetailF1: ImageView = view.findViewById(R.id.flagDetailF1) //Flag resource
+        val session1Date : TextView = view.findViewById(R.id.session1Date)
+        val session2Date : TextView = view.findViewById(R.id.session2Date)
+        val session3Date : TextView = view.findViewById(R.id.session3Date)
+        val session4Date : TextView = view.findViewById(R.id.session4Date)
+        val session5Date : TextView = view.findViewById(R.id.session5Date)
         val sessionTimeTv = arrayOf(R.id.session1Time, R.id.session2Time, R.id.session3Time,
-        R.id.session4Time, R.id.session5Time)
-        val session2 : TextView = findViewById(R.id.session2) // Practice 2 / Sprint Quali
-        val session3 : TextView = findViewById(R.id.session3) // Practice 3 / Practice 2
-        val session4 : TextView = findViewById(R.id.session4) // Quali / Sprint Race
-
-
+            R.id.session4Time, R.id.session5Time)
+        val session2 : TextView = view.findViewById(R.id.session2) // Practice 2 / Sprint Quali
+        val session3 : TextView = view.findViewById(R.id.session3) // Practice 3 / Practice 2
+        val session4 : TextView = view.findViewById(R.id.session4) // Quali / Sprint Race
         //Intent
         val weekendDate : String = race.dateF1 // Date of F1(ex. 22-24 JUN)
         val trackString : String = race.trackF1
@@ -56,11 +55,6 @@ class RaceDetailActivity : AppCompatActivity() {
         trackDateF1.text = weekendDate
         trackDetailF1.text = trackString
 
-        //------------------------------------------------------------------------------------------------------------
-        /*Binding Session 1-5 date.
-        Race weekend duration is 3 days(ex. 24-26 NOV) Session 1-2 is in 1st day, session 3-4
-        is in 2nd day, session 5 is in 3rd day of weekend.*/
-
 
         //Setting Session 1,2 date
         // Extract first date and name of month from string
@@ -68,7 +62,7 @@ class RaceDetailActivity : AppCompatActivity() {
         val testString2 : String = weekendDate
         // Case when weekend happens in 2 different months(ex.SEP-OCT)
         if(testString2.length >= 10){
-            testString = weekendDate.take(2) + " " + weekendDate.substring(6,9)
+           testString = weekendDate.take(2) + " " + weekendDate.substring(6,9)
         }
         session1Date.text = testString
         session2Date.text = testString
@@ -86,8 +80,8 @@ class RaceDetailActivity : AppCompatActivity() {
             val testInt = testString.substring(1,2).toInt() - 1
             testString = testString.take(1) + testInt.toString() + " " + weekendDate.takeLast(3)
         }
-        else{
-            testString = (testString.substring(0,2).toInt() - 1).toString() + " " + weekendDate.takeLast(3)
+       else{
+           testString = (testString.substring(0,2).toInt() - 1).toString() + " " + weekendDate.takeLast(3)
             //if result number in date is single digit
             if(testString.length == 5){
                 testString = "0" + testString
@@ -95,8 +89,8 @@ class RaceDetailActivity : AppCompatActivity() {
         }
         session3Date.text = testString
         session4Date.text = testString
-    //------------------------------------------------------------------------------------------------------------
-    // Sync with user local time
+        //------------------------------------------------------------------------------------------------------------
+        // Sync with user local time
 
         val myTimeZone = TimeZone.getTimeZone("Europe/Riga") // add tim ezone
         val currentTime = Calendar.getInstance() // add calendar library
@@ -106,20 +100,7 @@ class RaceDetailActivity : AppCompatActivity() {
             currentTime.set(Calendar.HOUR_OF_DAY, sessionTimeString[i].take(2).toInt()) // HOUR AND MINUTES ARE INT VAR
             currentTime.set(Calendar.MINUTE,(sessionTimeString[i].takeLast(2)).toInt())
             val dateTime = simpleDateFormat.format(currentTime.time).toString() //date is displayed with selected date format and time zone
-            findViewById<TextView>(sessionTimeTv[i]).setText(dateTime)
+            view.findViewById<TextView>(sessionTimeTv[i]).setText(dateTime)
         }
-    }
-    //Action bar overriding
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.app_menu, menu)
-        return true
-    }
-    //Adding icons to action bar
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.miAuthor -> Toast.makeText(this,"Author of this app is Olegs Pliska", Toast.LENGTH_LONG).show()
-            R.id.miTheme -> Toast.makeText(this,"Switching to light/dark theme(IN PROGRESS)", Toast.LENGTH_LONG).show()
-        }
-        return true
     }
 }
