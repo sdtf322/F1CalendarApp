a5f27d665686c3286ca86b89f8482b61684a8327
Olegs Pliska <olegpliska1@gmail.com>
2022-04-29 13:21:07 +0300

    removed unused imports

--- app/src/main/java/com/example/f1calendarOP/RaceDetailAdapter.kt
+++ app/src/main/java/com/example/f1calendarOP/RaceDetailAdapter.kt
@@ -1,15 +1,10 @@
 package com.example.f1calendarOP
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import java.lang.IllegalArgumentException
 
-// TODO:
-// -add views in detail fragment
 
 class RaceDetailAdapter : RecyclerView.Adapter<RaceDetailAdapterViewHolder>() {
 
