6e09833b6043bec7bdd2bbe9070217a78efe5cf5
Olegs Pliska <olegpliska1@gmail.com>
2022-05-03 09:12:44 +0300

    Changed DetailViewHolder bind function

--- app/src/main/java/com/example/f1calendarOP/RaceDetailViewHolder.kt
+++ app/src/main/java/com/example/f1calendarOP/RaceDetailViewHolder.kt
@@ -9,28 +9,41 @@ import androidx.recyclerview.widget.RecyclerView
 sealed class RaceDetailViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     class HeaderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val headerTrack : TextView = itemView.findViewById(R.id.tvTrackDetailF1)
+        private val headerDate : TextView = itemView.findViewById(R.id.tvDateF1_2)
+        private val headerFlag : ImageView = itemView.findViewById(R.id.flagDetailF1)
          fun bind(item: RaceDetailModel.Header) {
-            itemView.findViewById<TextView>(R.id.tvTrackDetailF1).text = item.track
-            itemView.findViewById<TextView>(R.id.tvDateF1_2).text = item.date
-            itemView.findViewById<ImageView>(R.id.flagDetailF1)?.setImageResource(item.flag)
+             headerTrack.text = item.track
+             headerDate.text = item.date
+             headerFlag.setImageResource(item.flag)
         }
     }
     class SessionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val sessionDate : TextView = itemView.findViewById(R.id.tvSessionDate)
+        private val sessionName : TextView = itemView.findViewById(R.id.tvSessionName)
+        private val sessionTime : TextView = itemView.findViewById(R.id.tvSessionTime)
          fun bind(item: RaceDetailModel.Session) {
-            itemView.findViewById<TextView>(R.id.tvSessionDate).text = item.sessionDate
-            itemView.findViewById<TextView>(R.id.tvSessionName).text = item.sessionName
-            itemView.findViewById<TextView>(R.id.tvSessionTime).text = item.sessionTime
+             sessionDate.text = item.sessionDate
+             sessionName.text = item.sessionName
+             sessionTime.text = item.sessionTime
         }
     }
     class CircuitViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val circuitImage : ImageView = itemView.findViewById(R.id.circuitImage)
+        private val firstYear : TextView = itemView.findViewById(R.id.tvCircuitFirstGPYear)
+        private val laps : TextView = itemView.findViewById(R.id.tvCircuitLaps)
+        private val length : TextView = itemView.findViewById(R.id.tvCircuitLength)
+        private val totalDistance : TextView = itemView.findViewById(R.id.tvCircuitDistance)
+        private val lapRecord : TextView = itemView.findViewById(R.id.tvCircuitLapRecord)
+        private val lapRecordOwner : TextView = itemView.findViewById(R.id.tvCircuitLapRecordHolder)
         fun bind(item: RaceDetailModel.Circuit) {
-            itemView.findViewById<ImageView>(R.id.circuitImage)?.setImageResource(item.circuitImage)
-            itemView.findViewById<TextView>(R.id.tvCircuitFirstGPYear).text = item.firstYear.toString()
-            itemView.findViewById<TextView>(R.id.tvCircuitLaps).text = item.laps.toString()
-            itemView.findViewById<TextView>(R.id.tvCircuitLength).text = item.circuitLength
-            itemView.findViewById<TextView>(R.id.tvCircuitDistance).text = item.raceDistance.toString()
-            itemView.findViewById<TextView>(R.id.tvCircuitLapRecord).text = item.lapRecord
-            itemView.findViewById<TextView>(R.id.tvCircuitLapRecordHolder).text = item.lapRecordOwner
+            circuitImage.setImageResource(item.circuitImage)
+            firstYear.text = item.firstYear.toString()
+            laps.text = item.laps.toString()
+            length.text = item.circuitLength
+            totalDistance.text = item.raceDistance.toString()
+            lapRecord.text = item.lapRecord
+            lapRecordOwner.text = item.lapRecordOwner
         }
     }
 }
