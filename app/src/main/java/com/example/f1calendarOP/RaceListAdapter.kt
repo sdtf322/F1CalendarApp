dcf8830be15d91e184cbc858908bf5d7bc666ca1
Olegs Pliska <olegpliska1@gmail.com>
2022-06-09 20:26:34 +0300

    adapted code to listAdapter

--- app/src/main/java/com/example/f1calendarOP/RaceListAdapter.kt
+++ app/src/main/java/com/example/f1calendarOP/RaceListAdapter.kt
@@ -10,13 +10,6 @@ import com.example.f1calendarOP.databinding.ItemRaceListBinding
     class RaceListAdapter(private val onClickListener: OnClickListener) :
         ListAdapter<Race, RaceListViewHolder.MyViewHolder>
             (raceDiffCallback) {
-
-    private val raceDiffer = AsyncListDiffer(this, raceDiffCallback)
-
-    var raceList : List<Race>
-        get() = raceDiffer.currentList
-        set(value) {raceDiffer.submitList(value)}
-
     @NonNull
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RaceListViewHolder.MyViewHolder {
         return RaceListViewHolder.MyViewHolder(ItemRaceListBinding.inflate(
@@ -26,14 +19,11 @@ import com.example.f1calendarOP.databinding.ItemRaceListBinding
         ))
     }
     override fun onBindViewHolder(holder: RaceListViewHolder.MyViewHolder, position: Int) {
-        val race = raceList[position]
+        val race = getItem(position)
         holder.itemView.setOnClickListener {
             onClickListener.onClick(race)
         }
-        holder.bind(raceList[position])
-    }
-    override fun getItemCount(): Int {
-        return raceList.size
+        holder.bind(race)
     }
     class OnClickListener(val clickListener: (race: Race) -> Unit) {
         fun onClick(race: Race) = clickListener(race)
