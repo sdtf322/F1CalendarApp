4e38fac25fcaf04d23a8ac0955b66f9cc6c80f85
Olegs Pliska <olegpliska1@gmail.com>
2022-06-14 21:27:32 +0300

    used binding for viewHolders

--- app/src/main/java/com/example/f1calendarOP/RaceDetailViewHolder.kt
+++ app/src/main/java/com/example/f1calendarOP/RaceDetailViewHolder.kt
@@ -13,43 +13,36 @@ sealed class RaceDetailViewHolder(itemView: View) : RecyclerView.ViewHolder(item
 
     class HeaderViewHolder(private val binding : ItemDetailHeaderBinding) :
         RecyclerView.ViewHolder(binding.root){
-        private val headerTrack : TextView = itemView.findViewById(R.id.tvTrackDetailF1)
-        private val headerDate : TextView = itemView.findViewById(R.id.tvDateF1_2)
-        private val headerFlag : ImageView = itemView.findViewById(R.id.flagDetailF1)
          fun bind(item: RaceDetailModel.Header) {
-             headerTrack.text = item.track
-             headerDate.text = item.date
-             headerFlag.setImageResource(item.flag)
+             with(binding){
+                 tvTrackDetailF1.text = item.track
+                 tvDateF12.text = item.date
+                 flagDetailF1.setImageResource(item.flag)
+             }
         }
     }
     class SessionViewHolder(private val binding : ItemDetailSessionBinding) :
         RecyclerView.ViewHolder(binding.root){
-        private val sessionDate : TextView = itemView.findViewById(R.id.tvSessionDate)
-        private val sessionName : TextView = itemView.findViewById(R.id.tvSessionName)
-        private val sessionTime : TextView = itemView.findViewById(R.id.tvSessionTime)
          fun bind(item: RaceDetailModel.Session) {
-             sessionDate.text = item.sessionDate
-             sessionName.text = item.sessionName
-             sessionTime.text = item.sessionTime
+             with(binding){
+                 tvSessionDate.text = item.sessionDate
+                 tvSessionName.text = item.sessionName
+                 tvSessionTime.text = item.sessionTime
+             }
         }
     }
     class CircuitViewHolder(private val binding : ItemDetailCircuitBinding) :
         RecyclerView.ViewHolder(binding.root){
-        private val circuitImage : ImageView = itemView.findViewById(R.id.circuitImage)
-        private val firstYear : TextView = itemView.findViewById(R.id.tvCircuitFirstGPYear)
-        private val laps : TextView = itemView.findViewById(R.id.tvCircuitLaps)
-        private val length : TextView = itemView.findViewById(R.id.tvCircuitLength)
-        private val totalDistance : TextView = itemView.findViewById(R.id.tvCircuitDistance)
-        private val lapRecord : TextView = itemView.findViewById(R.id.tvCircuitLapRecord)
-        private val lapRecordOwner : TextView = itemView.findViewById(R.id.tvCircuitLapRecordHolder)
         fun bind(item: RaceDetailModel.Circuit) {
-            circuitImage.setImageResource(item.circuitImage)
-            firstYear.text = item.firstYear.toString()
-            laps.text = item.laps.toString()
-            length.text = item.circuitLength
-            totalDistance.text = item.raceDistance.toString()
-            lapRecord.text = item.lapRecord
-            lapRecordOwner.text = item.lapRecordOwner
+            with(binding){
+                circuitImage.setImageResource(item.circuitImage)
+                tvCircuitFirstGPYear.text = item.firstYear.toString()
+                tvCircuitLaps.text = item.laps.toString()
+                tvCircuitLength.text = item.circuitLength
+                tvCircuitDistance.text = item.raceDistance.toString()
+                tvCircuitLapRecord.text = item.lapRecord
+                tvCircuitLapRecordHolder.text = item.lapRecordOwner
+            }
         }
     }
 }
--- app/src/main/java/com/example/f1calendarOP/RaceListViewHolder.kt
+++ app/src/main/java/com/example/f1calendarOP/RaceListViewHolder.kt
@@ -1,8 +1,6 @@
 package com.example.f1calendarOP
 
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.f1calendarOP.databinding.ItemRaceListBinding
@@ -12,19 +10,16 @@ sealed class RaceListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemVi
     class MyViewHolder(private val binding: ItemRaceListBinding)
         : RecyclerView.ViewHolder(binding.root) {
 
-            private val trackF1 = itemView.findViewById<TextView>(R.id.trackF1)
-            private val dateF1 = itemView.findViewById<TextView>(R.id.dateF1)
-            private val flagImage = itemView.findViewById<ImageView>(R.id.flagImage)
             fun bind(race : Race){
-                trackF1.text = race.raceName
-                dateF1.text = race.weekendDate
+                binding.trackF1.text = race.raceName
+                binding.dateF1.text = race.weekendDate
 
                 Glide.with(itemView)
                     .load(race.flagImage)
                     .placeholder(R.drawable.flag_unknown)
                     .error(R.drawable.flag_error)
                     .fallback(R.drawable.flag_error)
-                    .into(flagImage)
+                    .into(binding.flagImage)
             }
         }
     }
