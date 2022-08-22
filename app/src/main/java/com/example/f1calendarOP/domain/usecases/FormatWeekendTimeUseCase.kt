6bf188bf70e84a7a703a78ed563c205eb16539bd
Olegs Pliska <olegpliska1@gmail.com>
2022-08-22 10:06:42 +0300

    added time sync (deprecated)

--- app/src/main/java/com/example/f1calendarOP/domain/usecases/FormatWeekendTimeUseCase.kt
+++ app/src/main/java/com/example/f1calendarOP/domain/usecases/FormatWeekendTimeUseCase.kt
@@ -5,13 +5,19 @@ import java.util.*
 
 class FormatWeekendTimeUseCase {
 
-        operator fun invoke(raceTime: String): String { // Detail screen
+    operator fun invoke(raceTime: String): String { // Detail screen
 
-            val timeInputFormatter = SimpleDateFormat("HH:mm:ss'Z'", Locale.ENGLISH)
-            val timeOutputFormatter = SimpleDateFormat("HH:mm", Locale.ENGLISH)
-            val parsedTime = timeInputFormatter.parse(raceTime)
+        val timeInputFormatter = SimpleDateFormat("HH:mm:ss'Z'", Locale.ENGLISH)
+        val timeOutputFormatter = SimpleDateFormat("HH:mm", Locale.ENGLISH)
+        val parsedTime = timeInputFormatter.parse(raceTime) // 12:00 (Date)
 
-            return timeOutputFormatter.format(parsedTime)
+        val abobaCalendar = Calendar.getInstance(TimeZone.getDefault())
+        val timeZoneOffset = ((abobaCalendar.get(Calendar.ZONE_OFFSET) +
+                abobaCalendar.get(Calendar.DST_OFFSET)) / (3600 * 1000)) // Difference in Hours (3)
 
-        }
+        parsedTime.hours = parsedTime.hours + timeZoneOffset
+
+        return timeOutputFormatter.format(parsedTime)
+
+    }
     }
