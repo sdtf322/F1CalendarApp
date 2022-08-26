9162a1a4d4eaa61762d9e935fa9e7cfd7e556069
Olegs Pliska <olegpliska1@gmail.com>
2022-08-26 09:27:42 +0300

    Added Dagger to detail screen

--- app/src/main/java/com/example/f1calendarOP/presentation/racedetail/RaceDetailViewModel.kt
+++ app/src/main/java/com/example/f1calendarOP/presentation/racedetail/RaceDetailViewModel.kt
@@ -3,36 +3,16 @@ package com.example.f1calendarOP.presentation.racedetail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.f1calendarOP.data.repository.CircuitRepositoryImpl
 import com.example.f1calendarOP.domain.models.RaceModel
 import com.example.f1calendarOP.domain.models.RaceDetailModel
-import com.example.f1calendarOP.domain.usecases.FormatWeekendDateUseCase
-import com.example.f1calendarOP.domain.usecases.FormatWeekendTimeUseCase
 import com.example.f1calendarOP.domain.usecases.GetRaceDetailByIdUseCase
 
-class RaceDetailViewModel(private val raceModel : RaceModel) : ViewModel() {
+class RaceDetailViewModel(
+    private val raceModel : RaceModel,
+    private val getRaceDetailByIdUseCase: GetRaceDetailByIdUseCase)
+    : ViewModel() {
 
     private val raceInfoMutable = MutableLiveData<ArrayList<RaceDetailModel>>()
-
-    private val getRaceDetailByIdUseCase by lazy {
-        GetRaceDetailByIdUseCase(
-            formatWeekendDateUseCase,
-            formatWeekendTimeUseCase,
-            circuitRepository
-        )
-    }
-
-    private val circuitRepository by lazy {
-        CircuitRepositoryImpl()
-    }
-
-    private val formatWeekendDateUseCase by lazy {
-        FormatWeekendDateUseCase()
-    }
-
-    private val formatWeekendTimeUseCase by lazy {
-        FormatWeekendTimeUseCase()
-    }
 
     fun getDetailInfo() {
         val raceDetailModels = getRaceDetailByIdUseCase(raceModel)
