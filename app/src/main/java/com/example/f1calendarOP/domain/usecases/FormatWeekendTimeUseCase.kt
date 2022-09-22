a87d1eeb2bd3b1ae2b6ddf1876c9e4f661134b3e
Olegs Pliska <olegpliska1@gmail.com>
2022-09-22 07:48:41 +0300

    added time sync

--- app/src/main/java/com/example/f1calendarOP/domain/usecases/FormatWeekendTimeUseCase.kt
+++ app/src/main/java/com/example/f1calendarOP/domain/usecases/FormatWeekendTimeUseCase.kt
@@ -5,13 +5,29 @@ import java.util.*
 
 class FormatWeekendTimeUseCase {
 
-        operator fun invoke(raceTime: String): String { // Detail screen
+//        operator fun invoke(raceTime: String): String { // Detail screen
+//
+//            val timeInputFormatter = SimpleDateFormat("HH:mm:ss'Z'", Locale.ENGLISH)
+//            val timeOutputFormatter = SimpleDateFormat("HH:mm", Locale.ENGLISH)
+//            val parsedTime = timeInputFormatter.parse(raceTime)
+//
+//            return timeOutputFormatter.format(parsedTime)
+//
+//        }
 
-            val timeInputFormatter = SimpleDateFormat("HH:mm:ss'Z'", Locale.ENGLISH)
-            val timeOutputFormatter = SimpleDateFormat("HH:mm", Locale.ENGLISH)
-            val parsedTime = timeInputFormatter.parse(raceTime)
+    operator fun invoke(raceTime: String): String { // Detail screen
 
-            return timeOutputFormatter.format(parsedTime)
+        val timeInputFormatter = SimpleDateFormat("HH:mm:ss'Z'", Locale.ENGLISH)
+        val timeOutputFormatter = SimpleDateFormat("HH:mm", Locale.ENGLISH)
+        val parsedTime = timeInputFormatter.parse(raceTime)
 
-        }
+        val tzCalendar = Calendar.getInstance(TimeZone.getDefault())
+        val timeZoneOffset = ((tzCalendar.get(Calendar.ZONE_OFFSET) +
+                tzCalendar.get(Calendar.DST_OFFSET)) / (3600 * 1000)) // Difference in Hours (3)
+
+        parsedTime.hours = parsedTime.hours + timeZoneOffset
+
+        return timeOutputFormatter.format(parsedTime)
+
     }
+}
