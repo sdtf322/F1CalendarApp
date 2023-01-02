93cbaa316920db737822b2537edcf832eca70f99
Olegs Pliska <olegpliska1@gmail.com>
2023-01-02 11:29:11 +0200

    removed unnecessary @NotNull

--- app/src/main/java/com/example/f1calendarOP/presentation/racelist/RaceListAdapter.kt
+++ app/src/main/java/com/example/f1calendarOP/presentation/racelist/RaceListAdapter.kt
@@ -2,7 +2,6 @@ package com.example.f1calendarOP.presentation.racelist
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.annotation.NonNull
 import androidx.recyclerview.widget.ListAdapter
 import com.example.f1calendarOP.databinding.ItemRaceListBinding
 import com.example.f1calendarOP.domain.models.RaceModel
@@ -11,7 +10,7 @@ import com.example.f1calendarOP.utils.raceModelDiffCallback
 class RaceListAdapter(private val onClickHelper : (RaceModel) -> Unit) :
         ListAdapter<RaceModel, RaceListViewHolder.MyViewHolder>
             (raceModelDiffCallback) {
-    @NonNull
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RaceListViewHolder.MyViewHolder {
         return RaceListViewHolder.MyViewHolder(
             ItemRaceListBinding.inflate(
